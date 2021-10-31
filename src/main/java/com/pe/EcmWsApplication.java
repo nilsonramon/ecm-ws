7664f0abc5c0011ea69371d8638864158a2f0961
nramonibarra <nilson.ramon@cordonbleu.edu.pe>
2021-10-30 23:56:35 -0500

    first commit

---
+++ src/main/java/com/pe/EcmWsApplication.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/EcmWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
